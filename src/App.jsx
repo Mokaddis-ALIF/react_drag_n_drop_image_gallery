55fea98c06e6d16daa0900b05c2c02ff94493f1a
Mokaddis-ALIF <mokaddis.alif@gmail.com>
2023-11-06 22:48:06 +0600

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import img1 from './assets/images/image-1.webp'
-import img2 from './assets/images/image-2.webp'
-import img3 from './assets/images/image-3.webp'
-import img4 from './assets/images/image-4.webp'
-import img5 from './assets/images/image-5.webp'
-import img11 from './assets/images/image-11.jpeg'
-import img10 from './assets/images/image-10.jpeg'
-import img7 from './assets/images/image-7.webp'
-import img8 from './assets/images/image-8.webp'
-import img9 from './assets/images/image-9.webp'
+import { useState } from "react";
+import img1 from "./assets/images/image-1.webp";
+import img2 from "./assets/images/image-2.webp";
+import img3 from "./assets/images/image-3.webp";
+import img4 from "./assets/images/image-4.webp";
+import img5 from "./assets/images/image-5.webp";
+import img11 from "./assets/images/image-11.jpeg";
+import img10 from "./assets/images/image-10.jpeg";
+import img7 from "./assets/images/image-7.webp";
+import img8 from "./assets/images/image-8.webp";
+import img9 from "./assets/images/image-9.webp";
 
-import './App.css'
-import ImageGallery from './ImageGallery/ImageGallery'
+import "./App.css";
+import ImageGallery from "./ImageGallery/ImageGallery";
 
 function App() {
   const [state, setState] = useState([
@@ -26,40 +26,59 @@ function App() {
     { id: 9, url: img8, checked: false, hover: false },
     { id: 10, url: img1, checked: false, hover: false },
     { id: 11, url: img3, checked: false, hover: false },
-
-  ])
+  ]);
   console.log(state);
 
-  const selectedFiles = state.filter(item => item.checked == true);
+  const selectedFiles = state.filter((item) => item.checked == true);
 
-  const deleteHandler = () => {
+  const handlerForDelete = () => {
     let data = [...state];
-    data = data.filter(item => item.checked != true);
-    setState(data)
-  }
+    data = data.filter((item) => item.checked != true);
+    setState(data);
+  };
+
+  const handlerForCheckbox = () => {
+    let data = [...state];
+    data = data.map((item) => ({ ...item, checked: false }));
+    setState(data);
+  };
 
   return (
     <>
       <div className="main">
-        <div className='body'>
+        <div className="body">
           <div className="header">
             <div>
-
-              {selectedFiles.length > 0 ? <span>{`${selectedFiles.length == 1 ? `${selectedFiles.length} file selected` :
-                `${selectedFiles.length} files selected`}`}</span> :
+              {selectedFiles.length > 0 ? (
+                <span>
+                  <input
+                    type="checkbox"
+                    checked={selectedFiles.length > 0}
+                    id="headerCheckbox"
+                    onChange={handlerForCheckbox}
+                  />
+                  <label htmlFor="headerCheckbox">
+                    {`${selectedFiles.length == 1
+                      ? `${selectedFiles.length} file selected`
+                      : `${selectedFiles.length} files selected`
+                      }`}
+                  </label>
+                </span>
+              ) : (
                 <span>Gallery</span>
-              }
-
+              )}
             </div>
-            {selectedFiles.length > 0 && <span onClick={deleteHandler} className='deleteFile' >Delete {`${selectedFiles.length == 1 ? 'file' : 'files'}`} </span>}
+            {selectedFiles.length > 0 && (
+              <span onClick={handlerForDelete} className="deleteFile">
+                Delete {`${selectedFiles.length == 1 ? "file" : "files"}`}{" "}
+              </span>
+            )}
           </div>
           <ImageGallery state={state} setState={setState} />
         </div>
-
-
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/ImageGallery/ImageGallery.jsx
+++ src/ImageGallery/ImageGallery.jsx
@@ -1,30 +1,25 @@
-import { useEffect, useRef, useState } from 'react'
-import './ImageGallery.css'
-import img from '../assets/images/placeholder.png'
+import { useRef, useState } from "react";
+import "./ImageGallery.css";
+import img from "../assets/images/placeholder.png";
 
 export default function ImageGallery({ state, setState }) {
-
     const dragItem = useRef();
     const dragOverItem = useRef();
 
-    const inputFileProp = useRef(null);
+    const inputImage = useRef(null);
 
-
-
+    const [dragging, setDragging] = useState(null);
 
     const dragStart = (e, position) => {
-
         dragItem.current = position;
-
+        setDragging(position);
     };
 
     const dragEnter = (e, position) => {
-
         dragOverItem.current = position;
     };
 
-    const drop = (e) => {
-
+    const drop = () => {
         const copyListItems = [...state];
         const dragItemContent = copyListItems[dragItem.current];
         copyListItems.splice(dragItem.current, 1);
@@ -33,86 +28,79 @@ export default function ImageGallery({ state, setState }) {
         dragOverItem.current = null;
         setState(copyListItems);
 
-
-
+        setDragging(null);
     };
 
     const checkedHandler = (e, id) => {
         console.log(id);
-        const imageIndex = state.findIndex(item => item.id == id);
-        const imageUpdate = state.find(item => item.id == id);
-        // console.log(imageUpdate);
+        const imageIndex = state.findIndex((item) => item.id == id);
+        const imageUpdate = state.find((item) => item.id == id);
         const copyListItems = [...state];
-        copyListItems[imageIndex] = { ...imageUpdate, checked: !imageUpdate.checked }
-        // console.log(copyListItems);
-        setState(copyListItems)
-    }
+        copyListItems[imageIndex] = {
+            ...imageUpdate,
+            checked: !imageUpdate.checked,
+        };
+        setState(copyListItems);
+    };
 
-    const handleFileChange = (event) => {
-        const file = event.target.files[0];
-        // console.log(file);
+    const handlerForImage = (event) => {
         const copyListItems = [...state];
-        copyListItems.push({ id: copyListItems.length + 1, url: URL.createObjectURL(event.target.files[0]), hover: false })
-        console.log(copyListItems);
-        setState(copyListItems)
-        event.target.files = null
+        copyListItems.push({
+            id: copyListItems.length + 1,
+            url: URL.createObjectURL(event.target.files[0]),
+            hover: false,
+        });
+        setState(copyListItems);
+        event.target.files = null;
     };
 
     return (
-        <div className='image-grid'>
-            {state.map((image, index) => (
+        <div className="image-grid">
+            {state.map((data, index) => (
                 <div
-                    className={`main-div ${index == 0 ? 'image-grid-col-2 image-grid-row-2' : ''}`}
+                    className={`main-div ${index == 0 ? "image-grid-col-2 image-grid-row-2" : ""
+                        } ${dragging === index ? "dragging" : ""}`}
                     key={index}
                     onDragStart={(e) => dragStart(e, index)}
                     onDragEnter={(e) => dragEnter(e, index)}
                     onDragEnd={drop}
-                    draggable={!image.checked}
-                    style={{ opacity: image.checked ? 0.6 : 1 }}
+                    draggable={!data.checked}
+                    style={{ opacity: data.checked ? 0.6 : 1 }}
                 >
-
-
-                    <label className="container" >
+                    <label className="container">
                         <input
-
-                            type="checkbox" checked={image.checked} onChange={e => checkedHandler(e, image.id)}></input>
+                            type="checkbox"
+                            checked={data.checked}
+                            onChange={(e) => checkedHandler(e, data.id)}
+                        ></input>
                         <span className="checkmark"></span>
                     </label>
 
+                    <img src={data.url} className="grid-img" />
 
-                    <img
-
-                        src={image.url}
-                        className="grid-img"
-                    />
-
-                    {
-                        !image.checked &&
+                    {!data.checked && (
                         <div className="overlay">
                             <div className="text"></div>
                         </div>
-                    }
+                    )}
                 </div>
-
-
-
             ))}
 
             <div>
-
-                <img src={img} className="grid-img" onClick={() => inputFileProp.current.click()} />
-
+                <img
+                    src={img}
+                    className="grid-img"
+                    onClick={() => inputImage.current.click()}
+                />
 
                 <input
                     type="file"
-                    onChange={e => handleFileChange(e)}
-                    ref={inputFileProp}
+                    onChange={(e) => handlerForImage(e)}
+                    ref={inputImage}
                     style={{ display: "none" }}
                     multiple
                 />
-
             </div>
-
         </div>
-    )
+    );
 }
